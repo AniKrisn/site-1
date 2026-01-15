37c05eef2c1190b61c69d4aa817b612b0c013809
Ani <anirudh.krishnan@hotmail.com>
2026-01-15 18:10:07 +0000

    edit specificity post

--- src/Specificity.tsx
+++ src/Specificity.tsx
@@ -8,18 +8,17 @@ function Specificity() {
       <div className="content post-content">
         <article className="post-text">
           <h1>Specificity</h1>
-          <p>1. Progress is the production of more specific ideas.</p>
           <p>
-            2. Any real domain is fractal - if something’s there, it keeps
-            going.
+            1. Any real domain is fractal; if something's there, it keeps going.
           </p>
-          <p>3. More specific ideas are more valuable (for a smaller group).</p>
-          <p>4. More specific ideas are harder to express.</p>
+          <p>2. Progress is the production of more specific ideas.</p>
+          <p>3. More specific ideas are harder to express.</p>
+          <p>4. More specific ideas are more valuable (for a smaller group).</p>
           <p>
             5. There is demand to experience, collect, create and share
             increasingly specific ideas.
           </p>
-          <p>6. Specific ideas are generated from specific world models.</p>
+          <p>6. Specific ideas are produced by specific world models.</p>
           <p>
             7. There is demand to produce increasingly specific world models
             (both natural and artificial).
